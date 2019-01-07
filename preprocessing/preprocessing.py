4ba2543ab2f8c98d92108e1a6515f3593c7efd5f
Mark Grennan <grennama@tcd.ie>
2019-01-07 10:27:28 +0000

    dropped columns with missing values

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -13,7 +13,22 @@ dfTrain['SalePrice'] = dfTrain['SalePrice'].apply(np.log)
 
 dfTrain['LotArea'] = dfTrain['LotArea'].apply(np.log) # convert to log scale
 
-# create n-1 dummy variables for 'MSSubClass' variable
-dfTrain = pd.get_dummies(dfTrain, columns=['MSSubClass'], prefix=['SubClass'],
- drop_first=True)
-print (dfTrain.info())
+# # create n-1 dummy variables for 'MSSubClass' variable
+# dfTrain = pd.get_dummies(dfTrain, columns=['MSSubClass'], prefix=['SubClass'],
+#  drop_first=True)
+
+
+######################## Missing Values ####################
+
+# impute mode for single missing value in Electrical column
+dfTrain['Electrical'].fillna(dfTrain['Electrical'].mode().iloc[0],
+                            inplace=True)
+
+# list of features with missing values
+featuresWithMissingValues = [feature for feature in dfTrain
+                                if dfTrain[feature].isna().sum() != 0]
+
+# drop columns with missing values
+dfTrain.drop(columns=featuresWithMissingValues, inplace=True)
+
+######################## END Missing Values ####################
