ce34ce0bb623298944953a05b30cbc14f531dd1b
Mark Grennan <grennama@tcd.ie>
2019-01-08 09:57:42 +0000

    removed some redundant lines and updated missing data

--- preprocessing/exploratory.py
+++ preprocessing/exploratory.py
@@ -27,8 +27,8 @@ dfTrain = pd.read_csv(dataTrain, index_col=0)
 #     drawBarchart(str(feature), title=str(feature) + "_barchart")
 
 # # print skewnewss and kurtosis
-# print ("Skew: ", dfTrain['TotRmsAbvGrd'].skew())
-# print ("Kurt: ", dfTrain['TotRmsAbvGrd'].kurt())
+# print ("Skew: ", dfTrain['TotalBsmtSF'].skew())
+# print ("Kurt: ", dfTrain['TotalBsmtSF'].kurt())
 
 # # print scatterPlots of livingAreas
 # prints scatterplots on top of one another
@@ -37,19 +37,12 @@ dfTrain = pd.read_csv(dataTrain, index_col=0)
 #     drawScatter(livingAreas[i], title=str(livingAreas[i]))
 
 # # calculate rsquared
-# x = dfTrain['1stFlrSF']
+# dfTrain['GrLivArea'] = dfTrain['GrLivArea'].apply(np.log)
+# dfTrain['SalePrice'] = dfTrain['SalePrice'].apply(np.log)
+# x = dfTrain['GrLivArea']
 # y = dfTrain['SalePrice']
 # slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
 # print ("r-squared: ", r_value**2)
-
-# ################ working on below....
-# print ("Original: ", dfTrain['YearBuilt'].head(10))
-# dfTrain['squaredYearBuilt'] = dfTrain['YearBuilt']**2
-# print ("-"*20)
-# print ("Squared: ", dfTrain['squaredYearBuilt'].head(10))
-#
-# sns.lmplot(x="squaredYearBuilt", y="SalePrice", data=dfTrain)
-# plt.show()
 
 # ################# Missing Values ################################
 #
@@ -61,14 +54,14 @@ dfTrain = pd.read_csv(dataTrain, index_col=0)
 #                                     for feature in featuresWithMissingValues}
 #
 # sorted column of features with the sum of their missing values
-totalNumberMissingValues = dfTrain.isna().sum().sort_values(ascending=False)
-
-# percentage of missing data
-percentMissingData = (dfTrain.isna().sum()/len(dfTrain.index)).sort_values(ascending=False)
-
-# table of missing data and percentage
-missingData = pd.concat([totalNumberMissingValues, percentMissingData],
-                        axis = 1, keys=['Total', 'Percentage'])
-print (missingData.head(20))
+# totalNumberMissingValues = dfTrain.isna().sum().sort_values(ascending=False)
+#
+# # percentage of missing data
+# percentMissingData = (dfTrain.isna().sum()/len(dfTrain.index)).sort_values(ascending=False)
+#
+# # table of missing data and percentage
+# missingData = pd.concat([totalNumberMissingValues, percentMissingData],
+#                         axis = 1, keys=['Total', 'Percentage'])
+# print (missingData.head(20))
 
 # ################# END Missing Values ##############################
