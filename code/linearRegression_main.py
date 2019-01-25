cb9d04c0c5215053a62ec4ba9508475e51695e29
Mark Grennan <grennama@tcd.ie>
2019-01-25 09:16:28 +0000

    tidied up submission format

--- code/linearRegression_main.py
+++ code/linearRegression_main.py
@@ -6,33 +6,36 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn import linear_model
 from datetime import datetime
 
-# train dataset
-dataset = '/home/markg/kaggle/house_prices/data/working/train_17_01_2019_1733.csv'
-df = pd.read_csv(dataset, index_col=0)
-
-# split into 80% training, 20% testing
-y = df['SalePrice']
-X = df.drop(columns = ['SalePrice'])
+# # train dataset
+# dataset = '/home/markg/kaggle/house_prices/data/working/train_25_01_2019_0857.csv'
+# df = pd.read_csv(dataset, index_col=0)
+#
+# # split into 80% training, 20% testing
+# y = df['SalePrice']
+# X = df.drop(columns = ['SalePrice'])
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.10)
-
-# build linear regression model
+#
+# # build linear regression model
 # regr = linear_model.LinearRegression()
-
-# train model
-# regr.fit(X_train, y_train)
-
-# # # cross-validation
+#
+# # cross-validation
 # scores = cross_val_score(regr, X, y, cv=10, n_jobs=-1)
-# print ("Accuracy: ", scores.mean())
+# print ("Error: ", 1 - scores.mean())
 
 ############################ submission ################################
 # test dataset
-dt = '/home/markg/kaggle/house_prices/data/working/test_19_01_2019_0805.csv'
+dt = '/home/markg/kaggle/house_prices/data/working/test_25_01_2019_0900.csv'
 test_df = pd.read_csv(dt, encoding='latin-1', index_col=0)
 
 # prepare Id for submission file
 Id = test_df['Id']
 test_df.drop(columns = ['Id'], inplace = True)
+
+# train dataset
+dataset = '/home/markg/kaggle/house_prices/data/working/train_25_01_2019_0857.csv'
+df = pd.read_csv(dataset, index_col=0)
+y = df['SalePrice']
+X = df.drop(columns = ['SalePrice'])
 
 # fit linear regression for submission
 regr = linear_model.LinearRegression()
